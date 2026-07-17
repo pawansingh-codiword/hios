9ffdc9ec29c6c731b3b9821e851bd5ea2386f311
SANDEEP7522 <sandeepsahu04082003@gmail.com>
2026-07-17 18:22:34 +0530

    adde dai search

--- src/app/robots.js
+++ src/app/robots.js
@@ -5,6 +5,26 @@ export default function robots() {
         userAgent: '*',
         allow: '/',
       },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ChatGPT-User',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
     ],
     sitemap: 'https://indianoccult.com/sitemap.xml',
   };
